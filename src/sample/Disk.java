656157085f9d5b475804f59e0d34a7a5ea8bbe10
Hansa Patrick <patrick_hansa@yahoo.com>
2019-05-24 20:35:52 +0300

    Add a Disk class

---
+++ src/sample/Disk.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.scene.shape.Rectangle;
+
+public class Disk extends Rectangle {
+    private double orgSceneX;
+    private double orgSceneY;
+    private double orgTranslateX;
+    private double orgTranslateY;
+
+    public double getOrgSceneX() {
+        return orgSceneX;
+    }
+
+    public void setOrgSceneX(double orgSceneX) {
+        this.orgSceneX = orgSceneX;
+    }
+
+    public double getOrgSceneY() {
+        return orgSceneY;
+    }
+
+    public void setOrgSceneY(double orgSceneY) {
+        this.orgSceneY = orgSceneY;
+    }
+
+    public double getOrgTranslateX() {
+        return orgTranslateX;
+    }
+
+    public void setOrgTranslateX(double orgTranslateX) {
+        this.orgTranslateX = orgTranslateX;
+    }
+
+    public double getOrgTranslateY() {
+        return orgTranslateY;
+    }
+
+    public void setOrgTranslateY(double orgTranslateY) {
+        this.orgTranslateY = orgTranslateY;
+    }
+}
