502a7b0400d66e60a9e4881aa154c4e122b77662
Hansa Patrick <patrick_hansa@yahoo.com>
2019-05-27 20:34:13 +0300

    Implement the drag gesture for a disk

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,54 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    public void setOnMouseEntered(MouseEvent e) {
+        Disk disk = (Disk) e.getSource();
+
+        disk.setCursor(Cursor.HAND);
+    }
+
+    @FXML
+    public void setOnMousePressed(MouseEvent e) {
+        Disk disk = (Disk) e.getSource();
+
+        disk.setMouseTransparent(true);
+
+        disk.setOrgSceneX(e.getSceneX());
+        disk.setOrgSceneY(e.getSceneY());
+        disk.setOrgTranslateX(disk.getTranslateX());
+        disk.setOrgTranslateY(disk.getTranslateY());
+    }
+
+    @FXML
+    public void setOnDragDetected(MouseEvent e) {
+        Disk disk = (Disk) e.getSource();
+
+        disk.startFullDrag();
+    }
+
+    @FXML
+    public void setOnMouseDragged(MouseEvent e) {
+        Disk disk = (Disk) e.getSource();
+
+        double offsetX = e.getSceneX() - disk.getOrgSceneX();
+        double offsetY = e.getSceneY() - disk.getOrgSceneY();
+        double newTranslateX = disk.getOrgTranslateX() + offsetX;
+        double newTranslateY = disk.getOrgTranslateY() + offsetY;
+
+        disk.setTranslateX(newTranslateX);
+        disk.setTranslateY(newTranslateY);
+    }
+
+    @FXML
+    public void setOnMouseReleased(MouseEvent e) {
+        Disk disk = (Disk) e.getSource();
+
+        disk.setMouseTransparent(false);
+    }
 }
